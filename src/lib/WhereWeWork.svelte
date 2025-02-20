15d649a0296d28e15db96b28767287d7b3cd4655
Garik Asplund <garik.asplund@gmail.com>
2025-02-19 23:34:30 -0800

    close menu on our properties

--- src/lib/WhereWeWork.svelte
+++ src/lib/WhereWeWork.svelte
@@ -1,9 +1,7 @@
-<div class="relative -mx-6 lg:-mx-8"> <!-- Negative margin matches parent padding -->
-    <arcgis-embedded-map
-        style="height:600px;"
-        item-id="d455e8431ce349e988ea4b2837a48d8b"
-        theme="light"
-        portal-url="https://wallres.maps.arcgis.com"
-        class="w-full"
-    ></arcgis-embedded-map>
-</div>
+<arcgis-embedded-map
+	style="height:600px;"
+	item-id="d455e8431ce349e988ea4b2837a48d8b"
+	theme="light"
+	portal-url="https://wallres.maps.arcgis.com"
+	class="w-full"
+></arcgis-embedded-map>
