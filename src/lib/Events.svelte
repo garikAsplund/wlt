3a42088f795d2e0bab8547cf64e12698aa5c4bc8
Garik Asplund <garik.asplund@gmail.com>
2025-08-01 17:10:05 -0700

    added annual picnic

--- src/lib/Events.svelte
+++ src/lib/Events.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { Calendar, MapPin, ArrowUpRight } from 'lucide-svelte';
-	import potluckPoster from '$lib/images/events/potluck_poster.jpg';
+	import picnicPic from '$lib/images/events/picnic.jpg';
 </script>
 
-<div class="flex w-full justify-center bg-[#e9ad91]/40 py-12 px-4">
+<div class="flex w-full justify-center bg-[#e9ad91]/40 px-4 py-12">
 	<article
 		class="flex w-full max-w-3xl flex-col overflow-hidden rounded-lg
            bg-white transition-shadow
@@ -14,36 +14,36 @@
 			<div class="mb-4 flex flex-wrap gap-4 text-gray-700">
 				<div class="flex items-center">
 					<Calendar class="mr-2 h-5 w-5" />
-					<time>Friday May 23 • 6:00 PM</time>
+					<time>Saturday, August 16 • 4:00 PM</time>
 				</div>
 				<div class="flex items-center">
 					<MapPin class="mr-2 h-5 w-5" />
 					<a
-						href="https://www.google.com/maps?hl=en&q=Liberty+Grange+%23613+Hall,+Joseph,+OR+97846,+USA"
+						href="https://maps.app.goo.gl/rVX6Ev3VTgGUjvaYA"
 						target="_blank"
 						class="hover:underline"
 					>
-						Liberty Grange
+						601 S Main St, Joseph, OR
 					</a>
 				</div>
 			</div>
 
-			<h3 class="mb-4 text-xl font-semibold text-gray-900">Community Potluck & Celebration</h3>
+			<h3 class="mb-4 text-xl font-semibold text-gray-900">Annual Picnic</h3>
 
 			<p class="mb-4 text-gray-600">
-				Please help us welcome our Nez Perce, Umatilla, Cayuse, Walla Walla, Warm Springs and Yakama
-				friends to Wallowa County!
+				Enjoy live music by the Backyard Rangers, local beef burgers, and drinks.
 			</p>
 			<p class="mb-4 text-gray-600">
-				This outdoor potluck celebration is a chance to connect with old friends and make new ones.
-				We’ll have drinks and burgers on the grill—please bring a side salad or dessert to share.
+				Please bring a side dish to share as well as your own plates and utensils to help make this
+				a zero-waste event.
 			</p>
+			<p class="mb-4 text-gray-600">RSVP <span class="select-all">info@wallowalandtrust.org</span></p>
 
 			<a
-				href={potluckPoster}
+				href={picnicPic}
 				target="_blank"
 				rel="noopener noreferrer"
-				class="group mt-6 inline-flex w-fit items-center gap-2 border-b border-current text-slate-900 hover:text-slate-600"
+				class="group inline-flex w-fit items-center gap-2 border-b border-current text-slate-900 hover:text-slate-600"
 			>
 				See the invite
 				<ArrowUpRight class="h-4 w-4 transition-all duration-300 group-hover:rotate-45 " />
@@ -53,7 +53,7 @@
 		<!-- right: thumbnail -->
 		<div class="md:w-1/3">
 			<enhanced:img
-				src="/static/images/events/preview.png"
+				src="/static/images/events/picnic_cover.jpg"
 				alt="Potluck invite"
 				class="h-48 w-full object-cover md:h-full"
 				loading="lazy"
