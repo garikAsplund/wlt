d4d0bb8ad96e44756f9ee177d1e883fc34bc647e
Garik Asplund <garik.asplund@gmail.com>
2025-08-20 20:16:52 -0700

    removed PhD

--- src/lib/Staff.svelte
+++ src/lib/Staff.svelte
@@ -19,7 +19,7 @@
 			]
 		},
 		{
-			name: 'Caitlin Rushlow, PhD',
+			name: 'Caitlin Rushlow',
 			title: 'Conservation Program Manager',
 			email: 'caitlin@wallowalandtrust.org',
 			image: caitlinImg,
