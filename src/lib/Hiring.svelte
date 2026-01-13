0c0162b8f56776d237c4059345a5b6fce9e856f3
Garik Asplund <garik.asplund@gmail.com>
2026-01-12 21:47:43 -0800

    added RFP for catering for gathering

--- src/lib/Hiring.svelte
+++ src/lib/Hiring.svelte
@@ -1,4 +1,6 @@
-<div class="flex flex-col items-center w-full justify-center bg-[#36636c]/40 px-4 py-12 space-y-4 dark:text-gray-200">
+<div
+	class="flex w-full flex-col items-center justify-center space-y-4 bg-[#36636c]/40 px-4 py-12 dark:text-gray-200"
+>
 	<p class="text-xl font-bold tracking-wide">We're hiring!</p>
 	<p>
 		Please click <a
@@ -6,13 +8,24 @@
 			target="_blank"
 			class="font-bold underline underline-offset-4 transition-all duration-350 hover:opacity-75"
 			>here</a
-		> to read about the <b>Development Director</b> position. 
-		</p>
-		<p>And click <a
+		>
+		to read about the <b>Development Director</b> position.
+	</p>
+	<p>
+		And click <a
 			href="/PDFs/Indigenous-Community-Liaison.pdf"
 			target="_blank"
 			class="font-bold underline underline-offset-4 transition-all duration-350 hover:opacity-75"
 			>here</a
-		> to learn more about our <b>Indigenous Community Liaison</b> role.
+		>
+		to learn more about our <b>Indigenous Community Liaison</b> role.
+	</p>
+	<p>
+		We are also accepting proposals for <a
+			href="/PDFs/CateringRFP2026.pdf"
+			target="_blank"
+			class="font-bold underline underline-offset-4 transition-all duration-350 hover:opacity-75"
+			>Catering Services for the Wallowa Gathering</a
+		> in May.
 	</p>
 </div>
