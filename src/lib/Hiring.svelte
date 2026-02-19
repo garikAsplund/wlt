ba0cc20dcc7359a974a37b3277e66ea5420475ce
Garik Asplund <garik.asplund@gmail.com>
2026-02-19 07:00:21 -0800

    take down job listings

--- src/lib/Hiring.svelte
+++ src/lib/Hiring.svelte
@@ -1,8 +1,8 @@
 <div
 	class="flex w-full flex-col items-center justify-center space-y-4 bg-[#36636c]/40 px-4 py-12 dark:text-gray-200"
 >
-	<p class="text-xl font-bold tracking-wide">We're hiring!</p>
-	<p>
+	<p class="text-xl font-bold tracking-wide">Come work with us!</p>
+	<!-- <p>
 		Please click <a
 			href="/PDFs/Development-Director.pdf"
 			target="_blank"
@@ -10,8 +10,8 @@
 			>here</a
 		>
 		to read about the <b>Development Director</b> position.
-	</p>
-	<p>
+	</p> -->
+	<!-- <p>
 		And click <a
 			href="/PDFs/Indigenous-Community-Liaison.pdf"
 			target="_blank"
@@ -19,9 +19,9 @@
 			>here</a
 		>
 		to learn more about our <b>Indigenous Community Liaison</b> role.
-	</p>
+	</p> -->
 	<p>
-		We are also accepting proposals for <a
+		We are accepting proposals for <a
 			href="/PDFs/CateringRFP2026.pdf"
 			target="_blank"
 			class="font-bold underline underline-offset-4 transition-all duration-350 hover:opacity-75"
